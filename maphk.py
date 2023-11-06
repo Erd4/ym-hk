14b5ca9c20b056d5abf3121770a55203ea6a2586
Erd4 <alexander.kekkas@gmail.com>
2023-11-06 19:29:14 +0800

    finished app

--- maphk.py
+++ maphk.py
@@ -4,157 +4,130 @@ import folium
 from folium.plugins import HeatMap
 
 st.set_page_config(
-    page_title="KL Competitor Heatmap",
+    page_title="HK Competitor Heatmap",
     page_icon="🧘🏻‍♀️",
     )
 
 #creating a title for the app
-st.title("Heatmap of KL's Yoga Market")
+st.title("Heatmap of HK's Yoga Market")
 
 # Create a base map centered around Kuala Lumpur
-m = folium.Map(location=[3.1390, 101.6869], zoom_start=11)
+m = folium.Map(location=[22.302711, 114.177216], zoom_start=12)
 
 # Your list of latitudes and longitudes
-data = {'The Flow Studio': [
-    (3.1274188,101.6745625),
-    (3.1519383363602462, 101.66553696882904),
-    (3.138249, 101.63008),
-    (3.052161, 101.671278)
-    ],
-    'karma yoga': [
-        (3.1318964,101.6711135)
-        ],
-    'Life Hot Yoga': [
-        (3.149766037831857, 101.59387235348427),
-        (3.167091133373805, 101.65190068232039)
-        ],
-    'Celebrity Fitness': [
-        (3.1301827,101.6710671),
-        (3.1512698118957534, 101.6169901603),
-        (3.0698452683269846, 101.69535177114773),
-        (3.1193243119004923, 101.67848784897004),
-        (3.1958333217962744, 101.63381297508522)
-        ],
-    'Urban Spring Pilates': [
-        (3.1384434073561533, 101.6688006580661),
-        (3.1682057077947823, 101.65239181115658)
-        ],
-    'AlignYoga': [
-        (3.052142720526545, 101.67918610039212)
-        ],
-    'Surya Yoga': [
-        (3.1212672,101.5954205),
-        (3.078323903350539, 101.69372597375367),
-        (3.0927007030010296, 101.68761344990328),
-        (3.0448612,101.622157),
-        (3.010448265835054, 101.53854430327854),
-        (3.0978648832843207, 101.75346413257392),
-        (3.017641745178442, 101.69612923218372)
-        ],
-    'Simplylife Yoga': [
-        (3.179423396637816, 101.68323264272207)
-        ],
-    'YU+ Studio': [
-        (3.0543866096330508, 101.67000526697564),
-        (3.036385337758758, 101.76508320930336)
-        ],
-    'Omsphere': [
-        (3.0531466874387894, 101.73909438046711)
-        ],
-    'Mysore Room': [
-        (3.171403844928022, 101.66593865348423)
-        ],
-    'Sanctum Wellness at EQ': [
-        (3.153035935376106, 101.7097521093033)
-        ],
-    'mYoga': [
-        (3.1285862842180845, 101.71357731573829)
-        ],
-    'Art Printing Works Yoga': [
-        (3.1244613625431485, 101.67427181573822)
-        ],
-    'Damai Studio and Cafe': [
-        (3.1662218740184365, 101.71750229581187)
-        ],
-    'Life n Fitness': [
-        (3.12873404090602, 101.63503775348423),
-        (3.095408586930257, 101.73852216697564),
-        (3.1408348512615536, 101.70754993813944)
-        ],
-    'Journey Within Studio': [
-        (3.1550800734689615, 101.59488056882898)
-        ],
-    'Yogic Spandana': [
-        (3.16200592618416, 101.74212885991994)
-        ]
-    }
+data = {
+  "Pure Group - Yoga": [
+    (22.2767088, 114.1682706),
+    (22.2876928, 114.212492),
+    (22.2831831, 114.1556444),
+    (22.2821988, 114.1839874),
+    (22.2787869, 114.1824394),
+    (22.3189021, 114.1687515),
+    (22.3247465, 114.1731786),
+    (22.2954015, 114.172262),
+    (22.2350322, 114.1980799),
+    (22.3126322, 114.2253942),
+    (22.2780477, 114.1654178)
+  ],
+  "TriAngel Yoga": [
+    (22.2768089, 114.1814348),
+    (22.3026987, 114.1725416),
+    (22.3112696, 114.2225645),
+    (22.3677743, 114.1157649),
+    (22.3895625, 114.2094383)
+  ],
+  "Miga Studio": [
+    (22.3385861, 114.1991489),
+    (22.339083434395754, 114.1479518268551)
+  ],
+  "Yoga Senses": [
+    (22.3106414, 114.2260401),
+    (22.3370334, 114.1505753)
+  ],
+  "Flex Studio": [
+    (22.2805175, 114.155957),
+    (22.2483994, 114.1688561)
+  ],
+  "Raj Yoga Studio": [
+    (22.2870183, 114.148489)
+  ],
+  "One Yoga Studio": [
+    (22.2752738, 114.1714249),
+    (22.2825151, 114.1559351),
+    (22.2921964, 114.1996991)
+  ],
+  "Ikigai HK": [
+    (22.2807062, 114.1567481),
+    (22.2779309, 114.1826668)
+  ],
+  "The Yoga Room": [
+    (22.2852936, 114.150751)
+  ],
+  "Flowga Studio": [
+    (22.2809283, 114.1552982)
+  ]
+}
 
-regions = {'Bangsar': [
-    (3.1274188,101.6745625),
-    (3.1318964,101.6711135),
-    (3.1301827,101.6710671),
-    (3.1384434073561533, 101.6688006580661)
-    ],
-    'Kota Damansara': [
-        (3.149766037831857, 101.59387235348427),
-        (3.1212672,101.5954205),
-        (3.1550800734689615, 101.59488056882898)],
-    'Bukit Bintang': [
-        (3.153035935376106, 101.7097521093033),
-        (3.1285862842180845, 101.71357731573829),
-        (3.1662218740184365, 101.71750229581187),
-        (3.1408348512615536, 101.70754993813944),
-        (3.16200592618416, 101.74212885991994)
+
+regions = {
+    'Causeway Bay': [
+        (22.2821988, 114.1839874),
+        (22.2787869, 114.1824394),
+        (22.2768089, 114.1814348),
+        (22.2779309, 114.1826668)
         ],
-    'Bukit Jalil': [
-        (3.052161, 101.671278),
-        (3.052142720526545, 101.67918610039212),
-        (3.0543866096330508, 101.67000526697564)
+    'Central': [
+        (22.2831831, 114.1556444),
+        (22.2805175, 114.155957),
+        (22.2870183, 114.148489),
+        (22.2825151, 114.1559351),
+        (22.2807062, 114.1567481),
+        (22.2852936, 114.150751),
+        (22.2809283, 114.1552982)
         ],
-    'Mont Kiara': [
-        (3.1519383363602462, 101.66553696882904),
-        (3.167091133373805, 101.65190068232039),
-        (3.1682057077947823, 101.65239181115658),
-        (3.171403844928022, 101.66593865348423)
+    'Kwun Tong': [
+        (22.3126322, 114.2253942),
+        (22.3112696, 114.2225645),
+        (22.3106414, 114.2260401)
         ],
-    'Mid-Valley': [
-        (3.1193243119004923, 101.67848784897004),
-        (3.1244613625431485, 101.67427181573822)
+    'Wan Chai': [
+        (22.2767088, 114.1682706),
+        (22.2752738, 114.1714249),
+        (22.2780477, 114.1654178)
         ],
-    'Kepong Village': [
-        (3.1958333217962744, 101.63381297508522)
+    'North Point': [
+        (22.2876928, 114.212492),
+        (22.2921964, 114.1996991)
         ],
-    'Puchong Jaya': [
-        (3.0448612,101.622157),
+    'Tsim Sha Tsui': [
+        (22.2954015, 114.172262),
+        (22.3026987, 114.1725416)
         ],
-    'Kota Kemuning': [
-        (3.010448265835054, 101.53854430327854)
+    'Mong Kok': [
+        (22.3189021, 114.1687515),
+        (22.3247465, 114.1731786)
         ],
-    'Cheras': [
-        (3.0978648832843207, 101.75346413257392),
-        (3.0531466874387894, 101.73909438046711),
-        (3.036385337758758, 101.76508320930336),
-        (3.095408586930257, 101.73852216697564)
+    'Lai Chi  Kok': [
+        (22.339083434395754, 114.1479518268551),
+        (22.3370334, 114.1505753)
         ],
-    'TTDI': [
-        (3.138249, 101.63008),
-        (3.1512698118957534, 101.6169901603),
-        (3.12873404090602, 101.63503775348423),
+    'Repulse Bay': [
+        (22.2350322, 114.1980799)
         ],
-    'Seri Kembangan': [
-        (3.017641745178442, 101.69612923218372)
+    'Tsuen Wan': [
+        (22.3677743, 114.1157649)
         ],
-    'Sentul': [
-        (3.179423396637816, 101.68323264272207)
+    'Shimen': [
+        (22.3895625, 114.2094383)
         ],
-    'Sri Petaling': [
-        (3.0698452683269846, 101.69535177114773),
-        (3.078323903350539, 101.69372597375367)
-    ],
-    'Kuchai': [
-        (3.0927007030010296, 101.68761344990328)
-    ]
-    }
+    'San Po Kong': [
+        (22.3385861, 114.1991489)
+        ],
+    'Wong Chuk Hang': [
+        (22.2483994, 114.1688561)
+        ]
+}
+
 
 #Function to find the region of a studio
 def find_region(lat, lon, regions):
@@ -164,12 +137,15 @@ def find_region(lat, lon, regions):
     return None
 
 # Function to find all studios for a given latitude and longitude
-def find_studios(lat, lon, data):
-    matching_studios = []
+def find_studios(lat, lon, data, formatted_studios):
+    matching_studios = set()  # Use a set to avoid duplicates
+    
     for studio, coords in data.items():
         if (lat, lon) in coords:
-            matching_studios.append(studio)
-    return matching_studios
+            # Add the studio name as it is, no star for duplicates
+            matching_studios.add(formatted_studios.get(studio, studio))
+
+    return list(matching_studios)  # Convert the set back to a list
 
 # Function to format regions and studios in columns
 def format_columns(regions_studios):
@@ -212,18 +188,27 @@ def create_streetview(lat,lon):
 # Radio button for Studios vs. Regions
 selection_type = st.radio("Select by:", ["Studios", "Regions"])
 
-# Initialize a dictionary to store region-wise studio lists
-region_studio_mapping = {region: [] for region in regions}
+# Initialize a dictionary to store region-wise studio lists with counts
+region_studio_mapping = {region: {} for region in regions}
 
 # Update the mapping based on data
 for studio, coords in data.items():
     for coord in coords:
         region = find_region(coord[0], coord[1], regions)
         if region:
-            region_studio_mapping[region].append(studio)
+            if studio not in region_studio_mapping[region]:
+                region_studio_mapping[region][studio] = 0
+            region_studio_mapping[region][studio] += 1
+
+# Now, let's create a new mapping that formats the studio names with stars if needed
+formatted_region_studio_mapping = {}
+for region, studios in region_studio_mapping.items():
+    formatted_studios = {}
+    for studio, count in studios.items():
+        formatted_studios[studio] = f"{studio}*" if count > 1 else studio
+    formatted_region_studio_mapping[region] = formatted_studios
 
 if selection_type == "Studios":
-    # Implementing the "Select All" checkbox for studios
     select_all = st.checkbox("Select all Studios")
     if select_all:
         selections = list(data.keys())
@@ -232,6 +217,7 @@ if selection_type == "Studios":
 
     # Combine data from all selected studios into a single list and add markers with popups
     combined_data = []
+    regions_studios = {region: [] for region in regions}  #Initialize empty list for each region
     for s in selections:
         combined_data.extend(data[s])
         for point in data[s]:
@@ -267,7 +253,7 @@ else:
     for r in selections:
         combined_data.extend(regions[r])
         for point in regions[r]:
-            studios = find_studios(point[0], point[1], data)
+            studios = find_studios(point[0], point[1], data, formatted_region_studio_mapping[r])
             studios_text = ', '.join(studios)
             streetview_url = create_streetview(point[0], point[1])
             popup_text = f"""
@@ -295,21 +281,25 @@ HeatMap(combined_data).add_to(m)
 # Save the map to an HTML file
 st_folium(m,height=600,  width=700)
 
+# Use formatted_region_studio_mapping for displaying in Streamlit
 if selection_type == "Studios":
     regions_studios = {}
     for r, coords in regions.items():
-        studios_in_region = [s for s in selections if s in region_studio_mapping[r]]
+        # Use the formatted names with stars for duplicate studios
+        studios_in_region = [s for s in selections if s.strip('*') in formatted_region_studio_mapping[r]]
         if studios_in_region:
             regions_studios[r] = studios_in_region
 
     if regions_studios:
-        st.write("**Selected Studios in Their Respective Region:**")
+        st.write("**Selected Studios in Their Respective Regions:**")
         st.markdown(format_columns(regions_studios), unsafe_allow_html=True)
+
 
 else:
     regions_studios = {}
     for r in selections:
-        studios_in_region = region_studio_mapping[r]
+        # Use the formatted names with stars
+        studios_in_region = formatted_region_studio_mapping[r]
         if studios_in_region:
             regions_studios[r] = studios_in_region
 
